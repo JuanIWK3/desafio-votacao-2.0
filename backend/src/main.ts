222fd7ce805d7bb0b2bf14c2ccc677e1bba351ab
juaniwk3 <juaniwk3@gmail.com>
2023-12-23 13:47:59 -0300

    feat: express

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,11 @@
-console.log("start")
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
