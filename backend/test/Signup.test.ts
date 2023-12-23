7ca4d7ab1d91ff4b562eec4df2dc279fc24542fc
juaniwk3 <juaniwk3@gmail.com>
2023-12-23 14:07:56 -0300

    test: create user

--- backend/test/Signup.test.ts
+++ backend/test/Signup.test.ts
@@ -1,3 +1,24 @@
-test("should work", async () => {
-    expect(1+1).toBe(2);
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+beforeAll(async () => {
+  await prisma.user.deleteMany({});
+});
+
+test("should create a user", async () => {
+  console.log(await prisma.user.findMany());
+
+  const inputSignup: Prisma.UserCreateInput = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@email.com`,
+    cpf: "97456321558",
+    password: "12345678",
+  };
+
+  const user = await prisma.user.create({
+    data: inputSignup,
+  });
+
+  expect(user.email).toBe(inputSignup.email);
 });
