1ea49d134a2eae1ffc3466ac88eb038737599f07
juaniwk3 <juaniwk3@gmail.com>
2023-12-23 13:59:28 -0300

    feat: logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import LoggerConsole from "./infra/logger/LoggerConsole";
 
 const app = express();
 
@@ -6,6 +7,8 @@ app.use(express.json())
 
 const PORT = 4000;
 
+const logger = new LoggerConsole()
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  logger.log(`Server listening on port ${PORT}`);
 });
